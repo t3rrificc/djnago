ff7f11911b9e1b0f4b240cadb5b9b9c3be35b5a9
unknown <fitoswisniewskii@wp.pl>
2022-11-23 11:10:50 +0100

    first commit

---
+++ learing_templates/basic_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(requuest):
+    context_dict= {'text':'hello world','number':100}
+    return render(requuest,'basic_app/index.html',context_dict)
+
+def other(request):
+    return render(request,'basic_app/other.html')
+
+def relative(request):
+    return render(request,'basic_app/relative_url_templates.html')
